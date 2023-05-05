2ba99138824db203a98e8b41e6842550188897af
Leah Cornelius <56726537+leahcornelius@users.noreply.github.com>
2023-05-05 13:28:16 +0100

    Begin config file (pinouts)

---
+++ firmware/include/config.h
@@ -0,0 +1,12 @@
+// Pinouts
+#define SR_CLOCK A5 
+#define SR_LATCH A4
+#define SR_OUTPUT_ENABLE A3
+#define SR_DATA A2
+
+// Logic levels
+#define SR_OE_ENABLE_LEVEL LOW // Active low
+#define SR_OE_DISABLE_LEVEL HIGH
+
+#define HV_ENABLE_LEVEL HIGH // Active high
+#define HV_DISABLE_LEVEL LOW
